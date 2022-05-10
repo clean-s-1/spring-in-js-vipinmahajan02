53d5afcabf2b355d2e7771202c6a3a695ef2deaf
vipinmahajan02 <57444643+vipinmahajan02@users.noreply.github.com>
2022-05-10 18:56:38 +0530

    Update statistics.js

--- app/statistics.js
+++ app/statistics.js
@@ -1,7 +1,21 @@
 module.exports=function getStatistics(numbers){
+   
+    let sum=0;
     let min=0;
     let max=0;
-    let avg=0;
     //implement the computation of statistics here
+    for(let i=0;i<numbers.length;i++){
+      sum=sum+numbers[i];
+      if(numbers[i]<min){
+        min=numbers[i];
+      }
+       if(numbers[i]>max){
+       max=numbers[i];
+       }
+    }
+    obj.average=sum/numbers.length;
+    obj.min=min;
+    obj.max=max;
+    return obj;
 }
 
